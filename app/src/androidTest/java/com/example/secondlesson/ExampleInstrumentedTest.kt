3ffa0b85f2e4083d67a9332ace8a335a3afb00a4
zhiest <daria.zhitnaya@gmail.com>
2022-01-21 19:06:54 +0300

    lab2

--- app/src/androidTest/java/com/example/secondlesson/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/secondlesson/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.firstlesson
+package com.example.secondlesson
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.firstlesson", appContext.packageName)
+        assertEquals("com.example.secondlesson", appContext.packageName)
     }
 }
